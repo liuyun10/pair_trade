a077e43e06ee60a09c11f3713cbd343ade117119
panxudong1024 <pan_xu_dong1024@yahoo.co.jp>
2019-12-18 21:21:11 +0900

    update

--- analysis.py
+++ analysis.py
@@ -1,14 +1,15 @@
-import openpyxl,sys
+import openpyxl,sys, datetime
 import setting, os, pairs
 import pandas as pd
 import pair_back_test
 import fileutil as ft
+from dateutil.relativedelta import relativedelta
 
 excel_file_name = 'pair_trade.xlsm'
 sheet_name_history = '取引履歴'
 sheet_name_open_position = '建玉管理'
 
-def main(targetYM=None):
+def main(targetYear=None, targetMonth=None):
     print('Aalysis main start!')
     file_name = os.path.join(setting.get_master_dir(), excel_file_name)
     workbook = openpyxl.load_workbook(file_name, data_only=True)
@@ -19,9 +20,8 @@ def main(targetYM=None):
         year = sheet.cell(row=i, column=27).value
         if (year is None):
             continue
-        record.ym = str(year) + str(sheet.cell(row = i, column =28).value)
-        if (targetYM is not None and targetYM != record.ym):
-            continue
+        record.year = str(year)
+        record.month = str(sheet.cell(row = i, column =28).value)
         record.sellCode = str(sheet.cell(row = i, column= 6).value)
         record.buyCode = str(sheet.cell(row = i + 1, column= 6).value)
         record_list.append(record)
@@ -31,7 +31,7 @@ def main(targetYM=None):
         record = TradeRecord()
         record.sellCode = str(open_position_sheet.cell(row = i, column= 6).value)
         record.buyCode = str(open_position_sheet.cell(row = i + 1, column= 6).value)
-        record.ym = 'open'
+        record.kbn = 'open'
         record_list.append(record)
 
     for record in record_list:
@@ -42,8 +42,20 @@ def main(targetYM=None):
             symb2 = record.sellCode
         file_name = symb1 + '_' + symb2 + '.csv'
 
-        ft.create_target_dir(os.path.join(setting.get_input_data_dir(), 'trade', record.ym))
-        csv_file_full_path = os.path.join(setting.get_input_data_dir(), 'trade', record.ym, file_name)
+        if (record.kbn != 'open'):
+            target_ymd = datetime.datetime(year=int(record.year), month=int(record.month), day=1)
+            ymd_edit = target_ymd + relativedelta(months=2)
+            if(datetime.datetime.now() > ymd_edit):
+                continue
+
+        if (record.kbn == 'open'):
+            ft.create_target_dir(os.path.join(setting.get_input_data_dir(), 'trade', record.kbn))
+            csv_file_full_path = os.path.join(setting.get_input_data_dir(), 'trade', record.kbn, file_name)
+        else:
+            ft.create_target_dir(os.path.join(setting.get_input_data_dir(), 'trade', record.year))
+            ft.create_target_dir(os.path.join(setting.get_input_data_dir(), 'trade', record.year, record.month))
+            csv_file_full_path = os.path.join(setting.get_input_data_dir(), 'trade', record.year, record.month, file_name)
+
         cacluate_needed_data(symb1, symb2, csv_file_full_path)
 
     print('Aalysis main end!')
@@ -80,16 +92,15 @@ def cacluate_needed_data(symb1, symb2, csv_file_full_path):
 
 class TradeRecord:
     id = ""
-    ym = ""
+    year=""
+    month=""
     sellCode = ""
     buyCode = ""
     name = ""
+    kbn = ""
     def printAll(self):
-        print(self.ym + self.sellCode + self.buyCode)
+        print(self.year + self.month + self.sellCode + self.buyCode)
 
 if __name__ == '__main__':
     args = sys.argv
-    if (len(args) < 2):
-        print('Please input target date! YYYYMM')
-        sys.exit()
-    main(args[1])
+    main()
